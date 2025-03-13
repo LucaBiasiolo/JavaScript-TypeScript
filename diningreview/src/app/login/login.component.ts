c606693e707fa5d0ca46019f29ef2fbf930a90c3
Luca Biasiolo <lcbiasiolo@gmail.com>
2025-03-13 17:06:01 +0100

    Add style to form and return to home after successful login

--- diningreview/src/app/login/login.component.ts
+++ diningreview/src/app/login/login.component.ts
@@ -1,11 +1,14 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, FormsModule, NgForm, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { UserService } from '../user.service';
 import { MatButton } from '@angular/material/button';
+import { Router, RouterModule } from '@angular/router';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-login',
-  imports: [FormsModule, MatButton, ReactiveFormsModule],
+  imports: [FormsModule, MatButton, MatFormFieldModule, MatInputModule,ReactiveFormsModule, RouterModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
@@ -15,7 +18,7 @@ export class LoginComponent implements OnInit{
   //loginForm!: FormGroup;
   loginFormReactive!: FormGroup
 
-  constructor(private userService: UserService, private formBuilder: FormBuilder){}
+  constructor(private userService: UserService, private formBuilder: FormBuilder, private router: Router){}
 
   /*onSubmit(form: NgForm){
     this.userService.login(form.value.username, form.value.password).subscribe(
@@ -36,6 +39,9 @@ export class LoginComponent implements OnInit{
     this.userService.login(this.loginFormReactive.value.username, this.loginFormReactive.value.password).subscribe(
       (loggedIn) =>{
         this.loggedIn = loggedIn;
+        if (this.loggedIn){
+          this.router.navigate([''])
+        }
       }
     )
   }
