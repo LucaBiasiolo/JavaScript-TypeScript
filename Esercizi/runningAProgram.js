23810e2a4f9b9244b97f3c48d9a5ff1fc54aafaa
Luca Biasiolo <lcbiasiolo@gmail.com>
2025-03-13 15:57:01 +0100

    Add exercises from codecademy

---
+++ Esercizi/runningAProgram.js
@@ -0,0 +1,8 @@
+let noun1 = 'Luca';
+let adjective = 'funny';
+let noun2 = 'monkey';
+let verb = 'fly';
+let noun3 = 'penguin';
+
+
+console.log(`The world's first ${noun1} was a very ${adjective} ${noun2} who loved to ${verb} while eating ${noun3} for every meal.`);
