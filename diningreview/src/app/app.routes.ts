f181f2aa0a35fef839a90a4d8188ba2e85cb1908
Luca Biasiolo <lcbiasiolo@gmail.com>
2025-03-13 15:53:56 +0100

    Create home component and moved some logic in it

--- diningreview/src/app/app.routes.ts
+++ diningreview/src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { CreateUserComponent } from './create-user/create-user.component';
+import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
+  { path: 'home', component: HomeComponent},
     { path: 'login', component: LoginComponent },
   { path: 'signup', component: CreateUserComponent },
-  { path: '', redirectTo: '/login', pathMatch: 'full' }
+  { path: '', redirectTo: '/home', pathMatch: 'full' }
 ];
