b63b1185307334fee0b96fe7a709beb5407b75ee
Luca Biasiolo <lcbiasiolo@gmail.com>
2025-03-13 15:55:03 +0100

    create user interface and skeleton of user form

--- diningreview/src/app/create-user/create-user.component.ts
+++ diningreview/src/app/create-user/create-user.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-create-user',
@@ -6,6 +7,22 @@ import { Component } from '@angular/core';
   templateUrl: './create-user.component.html',
   styleUrl: './create-user.component.css'
 })
-export class CreateUserComponent {
+export class CreateUserComponent implements OnInit{
 
+  createUserForm!: FormGroup
+
+  constructor(private formBuilder: FormBuilder){}
+
+  ngOnInit(): void {
+      this.createUserForm = this.formBuilder.group({
+        username: ['', Validators.required],
+        password: ['', Validators.required],
+        city: [''],
+        zipcode: [''],
+        state: [''],
+        interestPeanutsAllergy: [false],
+        interestEggAllergy: [false],
+        interestDairyAllergy: [false]
+      })
+  }
 }
---
+++ diningreview/src/app/interfaces.ts
@@ -0,0 +1,11 @@
+export interface IUser{
+    username: string;
+    password: string;
+    city: string;
+    zipcode: string;
+    state: string;
+    isAdmin: boolean;
+    interestPeanutAllergy: boolean;
+    interestEggAllergy: boolean;
+    interestDairyAllergy: boolean;
+}
