fe12b3c24539de3ba80cd53f18230036e0ae6c2b
Luca Biasiolo <lcbiasiolo@gmail.com>
2025-03-13 17:04:36 +0100

    Change button type

--- diningreview/src/app/home/home.component.ts
+++ diningreview/src/app/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { MatButton } from '@angular/material/button';
 import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [RouterModule],
+  imports: [RouterModule, MatButton],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
