b66cffde5d6cce582d846b46a015b27ce5d519f3
Luca Biasiolo <lcbiasiolo@gmail.com>
2025-03-19 16:48:51 +0100

    add tests for HTTP methods

--- dining-review-be-3/src/app.ts
+++ dining-review-be-3/src/app.ts
@@ -6,6 +6,20 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
+app.post('/', (req, res) =>{
+  res.send('Test POST request from client');
+})
+
+app.put('/', (req, res) =>{
+  res.send('Test PUT request from client');
+})
+
+app.delete('/', (req, res) =>{
+  res.send('Test DELETE request from client');
+})
+
+app.use(express.static('public'))
+
 app.listen(port, () => {
   return console.log(`Express is listening at http://localhost:${port}`);
 });
