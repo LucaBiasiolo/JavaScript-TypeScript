22bea4c0767960059f38df69c681699865855924
Luca Biasiolo <lcbiasiolo@gmail.com>
2025-03-24 16:04:41 +0100

    Implement rock-paper-scissors

---
+++ games/src/app/rock-paper-scissors.service.ts
@@ -0,0 +1,53 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject, map } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RockPaperScissorsService {
+
+  public computerChoice: BehaviorSubject<number> = new BehaviorSubject<number>(-1);
+
+  constructor() { }
+
+  playRPS(userChoice: number): string{
+    const computerChoice: number = Math.floor(Math.random() *3);
+    this.computerChoice.next(computerChoice);
+    let winner: number = this.getWinner(userChoice, computerChoice);
+    return this.getPhraseFromWinner(winner, this.getWordFromNumber(userChoice), this.getWordFromNumber(computerChoice));
+  }
+
+  private getWinner(userPick: number, computerPick: number): number{
+    let winnerMatrix: number[][] = [[0,2,1],[1,0,2],[2,1,0]]; // 0=tie, 1=human player wins, 2=computer wins
+    return winnerMatrix[userPick][computerPick];
+  }
+
+  private getPhraseFromWinner(winner: number, userWord: string, computerWord: string):string{
+    let mapWinnerToPhrase: Map<number, string> = new Map<number, string>();
+    mapWinnerToPhrase.set(0, "It's a tie!");
+    mapWinnerToPhrase.set(1, `You won! ${userWord} beats ${computerWord}`);
+    mapWinnerToPhrase.set(2, `You lost! ${userWord} is beaten by ${computerWord}`);
+    return mapWinnerToPhrase.get(winner) || "Unknown result";
+  }
+
+  private getWordFromNumber(number: number): string{
+    let mapNumberToWord: Map<number, string> = new Map<number, string>();
+    mapNumberToWord.set(0,"Rock");
+    mapNumberToWord.set(1,"Paper");
+    mapNumberToWord.set(2,"Scissors");
+    return mapNumberToWord.get(number) || "Unknown code";
+  }
+}
+
+/*          System.out.println("Do you wish to play again? y/n");
+            String userContinue = scanner.nextLine().toLowerCase();
+            while (!userContinue.equals("n") && !userContinue.equals("y")) {
+                System.out.println("Please insert y to continue or n to exit the program");
+                userContinue = scanner.nextLine();
+            }
+            if (userContinue.equals("n")) {
+                System.out.println("Bye!");
+                scanner.close();
+                break;
+            }
+*/
--- games/src/app/rock-paper-scissors/rock-paper-scissors.component.ts
+++ games/src/app/rock-paper-scissors/rock-paper-scissors.component.ts
@@ -1,11 +1,36 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { RockPaperScissorsService } from '../rock-paper-scissors.service';
 
 @Component({
   selector: 'app-rock-paper-scissors',
-  imports: [],
+  imports: [MatButtonModule],
   templateUrl: './rock-paper-scissors.component.html',
   styleUrl: './rock-paper-scissors.component.css'
 })
 export class RockPaperScissorsComponent {
 
+  public userChoice: number = -1;
+  public computerChoice: number = -1;
+  public winnerPhrase: string = '';
+
+  constructor(private rockPaperScissorsService: RockPaperScissorsService){}
+
+  playRPS(userChoice: number){
+    this.userChoice = userChoice;
+    this.winnerPhrase = '';
+    this.computerChoice = -1;
+      const winnerPhrase = this.rockPaperScissorsService.playRPS(userChoice);
+      const computerChoice = this.rockPaperScissorsService.computerChoice.value;
+
+      // wait for 1 and 2 seconds to improve user's game experience
+      setTimeout(() => {
+        this.computerChoice = computerChoice;
+      }, 1000);
+      setTimeout(() => {
+        
+        this.winnerPhrase = winnerPhrase;
+
+      }, 2000);
+  }
 }
