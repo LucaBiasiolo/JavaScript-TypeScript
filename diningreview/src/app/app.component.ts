d27a76f0f83567cf792073550edf53e12aaf0a37
Luca Biasiolo <lcbiasiolo@gmail.com>
2025-04-03 18:33:32 +0200

    Delete restaurant list from app component

--- diningreview/src/app/app.component.ts
+++ diningreview/src/app/app.component.ts
@@ -1,23 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterModule, RouterOutlet } from '@angular/router';
-import { UserService } from './user.service';
-import { RestaurantListComponent } from "./restaurant-list/restaurant-list.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterModule, RestaurantListComponent],
+  imports: [RouterOutlet, RouterModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent implements OnInit{
+export class AppComponent{
   title = 'diningreview';
-  loggedIn = false;
-
-  constructor(private userService: UserService){}
-
-  ngOnInit(): void {
-      this.userService.loggedIn.subscribe((loggedIn) =>{
-        this.loggedIn = loggedIn;
-      })
-  }
 }
