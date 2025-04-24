681b13fb5fc41fdadde2e3d2ae8735a30746d03f
Luca Biasiolo <lcbiasiolo@gmail.com>
2025-04-24 12:03:19 +0200

    Improve style in chessboard

--- games/src/app/chess/chess.component.ts
+++ games/src/app/chess/chess.component.ts
@@ -13,33 +13,39 @@ export class ChessComponent implements OnInit {
   gameMatrix: string[][] = Array.from({ length: 8 }, () => Array(8).fill('')); //8 rows, 8 columns 
 
   public ngOnInit(): void {
+    this.initializeBoard(); 
+  }
+
+  public initializeBoard(){
     // Pawns
     for (let i = 0; i < 8; i++) {
-      this.gameMatrix[6][i] = "♙";
-      this.gameMatrix[1][i] = "♙";
-    }// Rooks
-    this.gameMatrix[0][0] = "♖";
-    this.gameMatrix[0][7] = "♖";
-    this.gameMatrix[7][0] = "♖";
+      this.gameMatrix[6][i] = "♙"; // White Pawn
+      this.gameMatrix[1][i] = "♟"; // Black Pawn
+    }
+    // Rooks
+    this.gameMatrix[0][0] = "♜"; // Black Rook
+    this.gameMatrix[0][7] = "♜";
+    this.gameMatrix[7][0] = "♖"; // White Rook
     this.gameMatrix[7][7] = "♖";
 
     // Knights
-    this.gameMatrix[0][1] = "♞";
+    this.gameMatrix[0][1] = "♞"; // Black Knight
     this.gameMatrix[0][6] = "♞";
-    this.gameMatrix[7][1] = "♞";
-    this.gameMatrix[7][6] = "♞";
+    this.gameMatrix[7][1] = "♘"; // White Knight
+    this.gameMatrix[7][6] = "♘";
 
     // Bishops
-    this.gameMatrix[0][2] = "♗";
-    this.gameMatrix[0][5] = "♗";
-    this.gameMatrix[7][2] = "♗";
+    this.gameMatrix[0][2] = "♝"; // Black Bishop
+    this.gameMatrix[0][5] = "♝";
+    this.gameMatrix[7][2] = "♗"; // White Bishop
     this.gameMatrix[7][5] = "♗";
+
     // Queens
-    this.gameMatrix[0][3] = "♕";
-    this.gameMatrix[7][3] = "♕";
+    this.gameMatrix[0][3] = "♛"; // Black Queen
+    this.gameMatrix[7][3] = "♕"; // White Queen
 
     // Kings
-    this.gameMatrix[0][4] = "♔";
-    this.gameMatrix[7][4] = "♔";
+    this.gameMatrix[0][4] = "♚"; // Black King
+    this.gameMatrix[7][4] = "♔"; // White King
   }
 }
