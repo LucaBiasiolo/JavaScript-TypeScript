d154ba0ed156438832a199b3e1b03216a364711b
Luca Biasiolo <lcbiasiolo@gmail.com>
2025-04-24 19:17:01 +0200

    First implementation of go board

--- games/src/app/go/go.component.ts
+++ games/src/app/go/go.component.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-go',
-  imports: [],
+  imports: [MatButtonModule],
   templateUrl: './go.component.html',
   styleUrl: './go.component.css'
 })
-export class GoComponent {
+export class GoComponent implements OnInit {
+  gameMatrix: string[][] = Array.from({ length: 9 }, () => Array(9).fill('')); //9 rows, 9 columns 
 
+ public ngOnInit(): void {
+    this.gameMatrix[0][0] =  "⚫";
+    this.gameMatrix[2][3] = "⚪";
+ }
+
+ public placeStone(row: number, column: number){
+    this.gameMatrix[row][column] = "⚪";
+ }
 }
