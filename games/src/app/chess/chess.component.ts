8e11e088eb93949e4d5be02621731bf6faba1a1f
Luca Biasiolo <lcbiasiolo@gmail.com>
2025-04-28 11:52:17 +0200

    Use Chess pieces classes to initialize board instead of icons

--- games/src/app/chess/chess.component.ts
+++ games/src/app/chess/chess.component.ts
@@ -1,5 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import { ChessPiece } from './Pieces/ChessPiece';
+import { Pawn } from './Pieces/Pawn';
+import { PieceColor } from './PieceColor';
+import { Rook } from './Pieces/Rook';
+import { Knight } from './Pieces/Knight';
+import { Bishop } from './Pieces/Bishop';
+import { Queen } from './Pieces/Queen';
+import { King } from './Pieces/King';
 
 @Component({
   selector: 'app-chess',
@@ -10,7 +18,7 @@ import { MatButtonModule } from '@angular/material/button';
 export class ChessComponent implements OnInit {
 
   player = 'White';
-  gameMatrix: string[][] = Array.from({ length: 8 }, () => Array(8).fill('')); //8 rows, 8 columns 
+  gameMatrix: ChessPiece[][] = Array.from({ length: 8 }, () => Array(8).fill('')); //8 rows, 8 columns 
 
   public ngOnInit(): void {
     this.initializeBoard(); 
@@ -19,34 +27,34 @@ export class ChessComponent implements OnInit {
   public initializeBoard(){
     // Pawns
     for (let i = 0; i < 8; i++) {
-      this.gameMatrix[6][i] = "♙"; // White Pawn
-      this.gameMatrix[1][i] = "♟"; // Black Pawn
+      this.gameMatrix[6][i] = new Pawn(PieceColor.WHITE); // White Pawn
+      this.gameMatrix[1][i] = new Pawn(PieceColor.BLACK); // Black Pawn
     }
     // Rooks
-    this.gameMatrix[0][0] = "♜"; // Black Rook
-    this.gameMatrix[0][7] = "♜";
-    this.gameMatrix[7][0] = "♖"; // White Rook
-    this.gameMatrix[7][7] = "♖";
+    this.gameMatrix[0][0] = new Rook(PieceColor.BLACK); // Black Rook
+    this.gameMatrix[0][7] = new Rook(PieceColor.BLACK);
+    this.gameMatrix[7][0] = new Rook(PieceColor.WHITE); // White Rook
+    this.gameMatrix[7][7] = new Rook(PieceColor.WHITE);
 
     // Knights
-    this.gameMatrix[0][1] = "♞"; // Black Knight
-    this.gameMatrix[0][6] = "♞";
-    this.gameMatrix[7][1] = "♘"; // White Knight
-    this.gameMatrix[7][6] = "♘";
+    this.gameMatrix[0][1] = new Knight(PieceColor.BLACK); // Black Knight
+    this.gameMatrix[0][6] = new Knight(PieceColor.BLACK);
+    this.gameMatrix[7][1] = new Knight(PieceColor.WHITE); // White Knight
+    this.gameMatrix[7][6] = new Knight(PieceColor.WHITE);
 
     // Bishops
-    this.gameMatrix[0][2] = "♝"; // Black Bishop
-    this.gameMatrix[0][5] = "♝";
-    this.gameMatrix[7][2] = "♗"; // White Bishop
-    this.gameMatrix[7][5] = "♗";
+    this.gameMatrix[0][2] = new Bishop(PieceColor.BLACK); // Black Bishop
+    this.gameMatrix[0][5] = new Bishop(PieceColor.BLACK);
+    this.gameMatrix[7][2] = new Bishop(PieceColor.WHITE); // White Bishop
+    this.gameMatrix[7][5] = new Bishop(PieceColor.WHITE);
 
     // Queens
-    this.gameMatrix[0][3] = "♛"; // Black Queen
-    this.gameMatrix[7][3] = "♕"; // White Queen
+    this.gameMatrix[0][3] = new Queen(PieceColor.BLACK); // Black Queen
+    this.gameMatrix[7][3] = new Queen(PieceColor.WHITE); // White Queen
 
     // Kings
-    this.gameMatrix[0][4] = "♚"; // Black King
-    this.gameMatrix[7][4] = "♔"; // White King
+    this.gameMatrix[0][4] = new King(PieceColor.BLACK); // Black King
+    this.gameMatrix[7][4] = new King(PieceColor.WHITE); // White King
   }
 
   public selectPiece(row: number, column: number){
