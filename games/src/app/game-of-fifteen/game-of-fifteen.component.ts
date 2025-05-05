4f50aa09925e827f3ffa7c9397b08a94712fb1c7
Luca Biasiolo <lcbiasiolo@gmail.com>
2025-05-05 17:40:03 +0200

    Add icon to return home

--- games/src/app/game-of-fifteen/game-of-fifteen.component.ts
+++ games/src/app/game-of-fifteen/game-of-fifteen.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-game-of-fifteen',
-  imports: [RouterModule, MatIconModule],
+  imports: [RouterModule, MatIconModule, MatButtonModule],
   templateUrl: './game-of-fifteen.component.html',
   styleUrl: './game-of-fifteen.component.css'
 })
