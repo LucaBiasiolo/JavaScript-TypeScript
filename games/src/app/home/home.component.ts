059dffd7fe1fee414c5f632faa76c3ab78806119
Luca Biasiolo <lcbiasiolo@gmail.com>
2025-05-05 17:41:32 +0200

    Improve home component using game objects

--- games/src/app/home/home.component.ts
+++ games/src/app/home/home.component.ts
@@ -1,9 +1,4 @@
-import { Component } from '@angular/core';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-import { MatSelectModule } from '@angular/material/select';
-import { RouterModule } from '@angular/router';
-
+import { Component } from '@angular/core'; import { MatFormFieldModule } from '@angular/material/form-field'; import { MatInputModule } from '@angular/material/input'; import { MatSelectModule } from '@angular/material/select'; import { RouterModule } from '@angular/router';
 @Component({
   selector: 'app-home',
   imports: [MatSelectModule, RouterModule, MatFormFieldModule, MatInputModule],
@@ -11,5 +6,13 @@ import { RouterModule } from '@angular/router';
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-
+  games = [{ name: 'Rock-Paper-Scissors', link: 'rock-paper-scissors' },
+  { name: 'Rock-Paper-Scissors-Lizard-Spock', link: 'rock-paper-scissors-lizard-spock' },
+  { name: 'Checkers', link: 'checkers' },
+  { name: 'Chess', link: 'chess' },
+  { name: 'Go', link: 'go' },
+  {name: 'Connect Four', link: 'connect-four'},
+  {name: 'Backgammon', link: 'backgammon'},
+  {name: 'Game of fifteen', link: 'game-of-fifteen'},
+  {name: 'Tic-tac-toe', link: 'tic-tac-toe'}]
 }
