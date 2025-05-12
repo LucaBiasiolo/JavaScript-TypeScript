5058bb6884330c4476a09b59fd9b79742444669c
Luca Biasiolo <lcbiasiolo@gmail.com>
2025-05-12 14:51:07 +0200

    Improve appearance of home component and game list

--- games/src/app/home/home.component.ts
+++ games/src/app/home/home.component.ts
@@ -1,7 +1,12 @@
-import { Component } from '@angular/core'; import { MatFormFieldModule } from '@angular/material/form-field'; import { MatInputModule } from '@angular/material/input'; import { MatSelectModule } from '@angular/material/select'; import { RouterModule } from '@angular/router';
+import { Component } from '@angular/core';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { RouterModule } from '@angular/router';
 @Component({
   selector: 'app-home',
-  imports: [MatSelectModule, RouterModule, MatFormFieldModule, MatInputModule],
+  imports: [MatSelectModule, RouterModule, MatFormFieldModule, MatInputModule, MatChipsModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
@@ -11,10 +16,12 @@ export class HomeComponent {
   { name: 'Checkers', link: 'checkers' },
   { name: 'Chess', link: 'chess' },
   { name: 'Go', link: 'go' },
-  {name: 'Connect Four', link: 'connect-four'},
-  {name: 'Backgammon', link: 'backgammon'},
-  {name: 'Game of fifteen', link: 'game-of-fifteen'},
-  {name: 'Tic-tac-toe', link: 'tic-tac-toe'},
-  {name: 'Klondike', link: 'Klondike'},
-  {name: 'Solitaire', link: 'solitaire'}].sort((game1, game2) => game1.name.localeCompare(game2.name));
+  { name: 'Connect Four', link: 'connect-four' },
+  { name: 'Backgammon', link: 'backgammon' },
+  { name: 'Game of fifteen', link: 'game-of-fifteen' },
+  { name: 'Tic-tac-toe', link: 'tic-tac-toe' },
+  { name: 'Klondike', link: 'Klondike' },
+  { name: 'FreeCell', link: 'free-cell' },
+  { name: 'Nine Men\'s morris', link: 'nine-mens-morris' },
+  { name: 'Domino', link: 'domino' }].sort((game1, game2) => game1.name.localeCompare(game2.name));
 }
