d997eab3834c2fd4fab854490fc5ae9d532e230d
Luca Biasiolo <lcbiasiolo@gmail.com>
2025-05-12 14:32:06 +0200

    Create components for new games

---
+++ games/src/app/domino/domino.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-domino',
+  imports: [],
+  templateUrl: './domino.component.html',
+  styleUrl: './domino.component.css'
+})
+export class DominoComponent {
+
+}
---
+++ games/src/app/nine-mens-morris/nine-mens-morris.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nine-mens-morris',
+  imports: [],
+  templateUrl: './nine-mens-morris.component.html',
+  styleUrl: './nine-mens-morris.component.css'
+})
+export class NineMensMorrisComponent {
+
+}
